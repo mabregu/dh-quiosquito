f394209ce4cb7afd3efb0428816b0a2e9b13c398
Martin <nmabregu@gmail.com>
2022-09-15 09:34:46 -0300

    add hanlder items and storage

--- src/public/js/cart.js
+++ src/public/js/cart.js
@@ -2,9 +2,14 @@ let cartPage = document.getElementById("cart");
 let cartBtn = document.getElementById("cart-btn");
 let cartBtnClose = document.getElementById("cart-btn-close");
 let addToCartBtn = document.getElementById("add-to-cart-btn");
+let cartList = document.getElementById("cart-list");
+let cartCountNotification = document.getElementById("cart-count-notification");
+let cartCountItems = document.getElementById("cart-count-items");
+
+addProductListToCart();
+notificationCart();
 
 cartBtn.onclick = () => {
-    console.log("cartBtn.onclick", cartPage.style.display);
     if (cartPage.style.display == "none" || cartPage.style.display == "") {
         cartPage.style.display = "block";
     } else {
@@ -13,7 +18,6 @@ cartBtn.onclick = () => {
 };
 
 cartBtnClose.onclick = () => {
-    console.log("cartBtnClose.onclick", cartPage.style.display);
     if (cartPage.style.display == "block") {
         cartPage.style.display = "none";
     }
@@ -25,11 +29,125 @@ if (addToCartBtn) {
         let product = {
             id: e.target.dataset.id,
             name: e.target.dataset.name,
+            description: e.target.dataset.description,
             price: e.target.dataset.price,
             currency: e.target.dataset.currency,
             image: e.target.dataset.image
         }
         
-        console.log("addToCartBtn.onclick", product);
+        updateOrCreateCart(product);        
     };
+}
+
+function addProductListToCart() {
+    let productList = getCart();
+
+    if (productList) {
+
+        productList.forEach(product => addProductItem(product));
+    }
+}
+
+function addProductItem(product) {
+    let li = `
+        <li id="cart-item-${product.id}" class="flex py-6 cart-item">
+            <div class="h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200">
+                <img src="${product.image}" 
+                    alt="${product.name}" 
+                    class="h-full w-full object-cover object-center"
+                >
+            </div>
+            <div class="ml-4 flex flex-1 flex-col">
+                <div>
+                    <div class="flex justify-between text-base font-medium text-gray-900">
+                    <h3>
+                        <a href="#">${product.name}</a>
+                    </h3>
+                    <p class="ml-4">${product.currency} ${product.price}</p>
+                    </div>
+                    <p class="mt-1 text-sm text-gray-500">${product.description}</p>
+                </div>
+                <div class="flex flex-1 items-end justify-between text-sm">
+                    <p class="text-gray-500">1</p>
+                    <div class="flex">
+                        <button 
+                            type="button" 
+                            class="font-medium text-indigo-600 hover:text-indigo-500"
+                            onclick="removeItemCart('cart-item-${product.id}')"
+                        >Remove</button>
+                    </div>
+                </div>
+            </div>
+        </li>
+    `;
+
+    cartList.innerHTML += li;
+}
+
+function removeItemCart(elementId) {
+    document.getElementById(elementId).remove();
+}
+
+function removeItems() {
+    let items = document.querySelectorAll('.cart-item');
+
+    for (let index = 0; index < items.length; index++) {
+        const item = items[index];
+        item.remove();
+    }
+
+    localStorage.clear();
+}
+
+function notificationCart() {
+    cartCountItems.innerText = getCountItemsInCart();
+
+    if (getCountItemsInCart()) cartCountNotification.style.backgroundColor = 'red';
+}
+
+function updateOrCreateCart(product) {
+    if (localStorage.getItem('cart')) {
+        let cart = JSON.parse(localStorage.getItem('cart'));
+        let indexProductInCart = cart.findIndex(cartItem => cartItem.productId == product.id);
+
+        if (indexProductInCart !== -1) {
+            cart[indexProductInCart].quantity++;
+        } else {
+            cart.push({
+                productId: product.id,
+                name: product.name,
+                description: product.description,
+                price: product.price,
+                currency: product.currency,
+                image: product.image,
+                quantity: 1
+            });
+        }
+
+        localStorage.setItem('cart', JSON.stringify(cart));
+    } else {
+        localStorage.setItem(
+            "cart",
+            JSON.stringify([{
+                productId: product.id,
+                name: product.name,
+                description: product.description,
+                price: product.price,
+                currency: product.currency,
+                image: product.image,
+                quantity: 1
+            }])
+        );
+    }
+
+    addProductListToCart();
+    notificationCart();
+}
+
+function getCountItemsInCart() {
+    return localStorage.getItem('cart') ? JSON.parse(localStorage.getItem('cart')).length : 0;
+}
+
+function getCart() {
+    return localStorage.getItem('cart') ? JSON.parse(localStorage.getItem('cart')) : null;
 }
