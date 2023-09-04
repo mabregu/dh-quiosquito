6ab756b4f1c2ff1452a1521bd4f1b5c9a82bbb67
Martin <nmabregu@gmail.com>
2023-09-03 22:47:15 -0300

    config

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -17,10 +17,10 @@ module.exports =
     "dialect": "mysql"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_DATABASE,
+    "host": process.env.DB_HOST,
+    "dialect": process.env.DB_MOTOR
   }
 }
